310c69cfb95c6526c217f4cc8d043f4b8be169a1
Stefania La Vattiata Sanchez <stefanialvs@gmail.com>
2020-10-18 20:59:31 -0400

    python envs

---
+++ src_py/setup.sh
@@ -0,0 +1,7 @@
+# dataviz packages
+conda create --name dataviz python=3.6
+conda activate dataviz
+conda install numpy
+conda install pandas
+conda install -c conda-forge jupyterlab
+conda deactivate
