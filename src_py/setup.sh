fab2e059993653f23acafabb06653f23d197c095
Stefania La Vattiata Sanchez <stefanialvs@gmail.com>
2020-10-20 23:22:41 -0400

    Zomato Api class and zcities_df generation

--- src_py/setup.sh
+++ src_py/setup.sh
@@ -3,5 +3,7 @@ conda create --name dataviz python=3.6
 conda activate dataviz
 conda install numpy
 conda install pandas
+conda install -c anaconda xlrd
 conda install -c conda-forge jupyterlab
+pip install python-Levenshtein
 conda deactivate
